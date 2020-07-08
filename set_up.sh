b4294567e380be2df8f668f9557d13fbe9175b78
Tai Duong <imbaggaarm@gmail.com>
2020-07-08 23:45:43 +0700

    [c] Create set up script

--- set_up.sh
+++ set_up.sh
@@ -1,2 +1,7 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Install python needed packages
+pip3 istall jedi
+pip3 install pynvim
+pip3 install yapf
+pip3 install pylint
